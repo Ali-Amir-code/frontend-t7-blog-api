5a587a959741480346f5f06920481eaaf941f11d
Ali Amir <muhammadaliamir24@gmail.com>
2025-08-09 20:16:31 +0500

    URL refactoring.

--- js/api.js
+++ js/api.js
@@ -1,4 +1,4 @@
-export const API_BASE_URL = 'http://localhost:5000/api';
+export const API_BASE_URL = 'https://t7-blog-api.onrender.com/api';
 
 function getToken() {
   return localStorage.getItem('token');
